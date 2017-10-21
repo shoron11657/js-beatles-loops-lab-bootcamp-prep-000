33cc8cd5b4c7ab470f706969e58352db1880f345
Kevin M <shoron@gmail.com>
2017-10-21 04:01:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var musicians = ['John Lennon','Paul McCartney', 'George Harrison', 'Ringo Starr
 var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
 
 function theBeatlesPlay(musicians, instruments) {
-  var whatTheyPlay = []
-  for (var i=0; i<4; i++){
-    console.log(musicians[i] 'plays' instruments[i])  }
-  return whatTheyPlay
+  var band = []
+  for (var i=0; i<musicians.length; i++){
+    band.push (musicians[i] ' plays ' instruments[i])}
+  return band
 }

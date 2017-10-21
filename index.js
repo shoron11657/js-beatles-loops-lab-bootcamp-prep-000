4af22ab1b9c01c9ba3cb6405a0ed7e53b8f7bbd7
Kevin M <shoron@gmail.com>
2017-10-21 04:03:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var musicians = ['John Lennon','Paul McCartney', 'George Harrison', 'Ringo Starr']
-var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
-
-function theBeatlesPlay(musicians, instruments) {
-  var band = []
-  for (var i=0; i<musicians.length; i++){
-    band.push (musicians[i] ' plays ' instruments[i])}
-  return band
+function theBeatlesPlay(musicians, instruments){
+  var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+  var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+  var band = new Array(4);
+  for (let i = 0; i < 4; i++) {
+    band[i] = musicians[i] + ' plays ' + instruments[i]
+  }
+  return band;
 }
